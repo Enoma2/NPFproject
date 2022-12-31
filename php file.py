83b045d868dc0ad6f45cf5dfee5272a614c9a1aa
Enoma <osaroboenoma72@gmail.com>
2022-12-31 14:03:05 +0100

    first commit

---
+++ php file.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x1=np.array([0.1,38.21,68.21,30.1,0.1])
+x2=np.array([10.1,48.21])
+x3=np.array([7.6,45.71])
+y2=np.array([39.42,17.42])
+y1=np.array([22.1,0.1,52.06,74.06,22.1])
+y3=np.array([35.09,13.09])
+x4=np.array([25.1,63.21])
+y4=np.array([65.4,43.4])
+x5=np.array([26.96,37.35,43.62,32.95,26.96])
+y5=np.array([36.62,30.62,41.01,47.01,36.62])
+plt.plot(x3,y3,'--',x4,y4, '--')
+plt.plot(x1,y1,x2,y2,x5,y5)
+Plt.title(" Agho Elvis ENG1809129 mechanical engineering MEE372" , loc="left" )
+Plt.title(" first angle isometric drawing " loc="left" )
+plt.show()
